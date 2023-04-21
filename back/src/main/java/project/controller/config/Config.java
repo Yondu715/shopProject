4278771bb4d39062506d58000102837daecdc379
MaximVorobev <maxv153218@gmail.com>
2023-04-21 22:12:52 +0700

    back structure

---
+++ back/src/main/java/project/controller/config/Config.java
@@ -0,0 +1,8 @@
+package project.controller.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class Config extends Application {}
+
